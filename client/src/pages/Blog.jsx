36e4d5d2fadced4fc9a27a0342ee7e6964589093
Harsh Thakur <meharshthakur.3365@gmail.com>
2025-08-19 14:11:10 +0530

    fixed comment component

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -37,7 +37,7 @@ const Blog = () => {
 
   const fetchComments = async () => {
     try {
-      const { data } = await axios.post('/api/blog/comments', { blog: id });
+      const { data } = await axios.post('/api/blog/comments', { blogId: id });
       if (data.success) {
         setComments(data.comments); // Assuming backend already filters only approved comments
         console.log("Approved comments received:", data.comments);
